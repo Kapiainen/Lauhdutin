57f9b52182f9b666b7e7b02243423b7ba8d248bb
Kapiainen <Kapiainen@users.noreply.github.com>
2017-02-06 01:07:14 +0200

    Updated RunTests batch file

--- Tests/RunTests.bat
+++ Tests/RunTests.bat
@@ -1,2 +1,8 @@
+@echo ============================= Running Python tests =============================
 @python RunTests.py
+@echo.
+@echo.
+::@echo ============================= Running Lua tests ================================
+::@echo.
+::@echo.
 @pause
