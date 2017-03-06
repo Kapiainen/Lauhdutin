2404bfce74759b10e1f42caad72bf8cb9e5a9ba2
Kapiainen <Kapiainen@users.noreply.github.com>
2017-03-06 12:36:33 +0200

    Version 2.5.0 (#29)
    - Added optional animations when clicking on a slot.
    - Added optional animations when hovering the mouse over a slot.
    - Added support for retaining information about games, which were previously detected and then uninstalled, for future use.
    - Added button separators to the toolbar.
    - Added 'R' next to the sorting icon when the sorting order is reversed.
    - Updated toolbar icons.
    - Updated settings skin.

--- Lauhdutin/@Resources/Frontend/BuildSkin.py
+++ Lauhdutin/@Resources/Frontend/BuildSkin.py
@@ -14,34 +14,196 @@ try:
         return None
 
     settings = read_json(os.path.join(ResourcePath, "settings.json"))
+    SLOT_WIDTH = int(settings.get("slot_width", 418))
+    SLOT_HEIGHT = int(settings.get("slot_height", 195))
+    SLOT_COUNT = int(settings.get("slot_count", 6))
+    ORIENTATION = settings.get("orientation", "vertical")
 
     with open(os.path.join(ResourcePath, "Frontend", "GameSlots.inc"),
               "w") as f:
         # Variables
-        if settings.get("orientation", "vertical") == "vertical":
+        if ORIENTATION == "vertical":
             f.write("""[Variables]
-ToolbarWidth=%s""" % (settings.get("slot_width", 418)))
+ToolbarWidth=%s
+SkinMaxWidth=%s
+SkinMaxHeight=%s""" % (SLOT_WIDTH, SLOT_WIDTH, SLOT_HEIGHT * SLOT_COUNT))
         else:
             f.write("""[Variables]
-ToolbarWidth=%s""" % (int(settings.get("slot_width", 418)) *
-                      int(settings.get("slot_count", 6))))
+ToolbarWidth=%s
+SkinMaxWidth=%s
+SkinMaxHeight=%s""" % (SLOT_WIDTH *
+                      SLOT_COUNT, SLOT_WIDTH * SLOT_COUNT, SLOT_HEIGHT))
         f.write("""
 SlotCount=%s
 SlotWidth=%s
 SlotHeight=%s
 SlotBackgroundColor=%s
 SlotTextColor=%s
-""" % (settings.get("slot_count", 6), settings.get("slot_width", 418),
-       settings.get("slot_height", 195), settings.get("slot_background_color",
-                                                      "0,0,0,196"),
-       settings.get("slot_text_color", "255,255,255,255")))
+""" % (
+        SLOT_COUNT,
+        SLOT_WIDTH,
+        SLOT_HEIGHT,
+        settings.get("slot_background_color", "0,0,0,196"),
+        settings.get("slot_text_color", "255,255,255,255")
+    )
+)
         i = 1
-        while i <= int(settings.get("slot_count", 6)):
+        while i <= SLOT_COUNT:
             f.write("""SlotImage%s=
 SlotName%s=
 SlotHighlightMessage%s=
 """ % (i, i, i))
             i += 1
+
+        f.write("""
+[ClickAnimation]
+Measure=Plugin
+Plugin=ActionTimer
+ActionList1=ResetVertical | Wait #FrameInterval# | Shrink1Vertical | Wait #FrameInterval# | Shrink2Vertical | Wait #FrameInterval# | Shrink3Vertical | Wait #FrameInterval# | BlankSlot | Wait #FrameInterval# | ResetVertical | Launch
+ActionList2=ResetVertical | Wait #FrameInterval# | ShiftLeft1 | Wait #FrameInterval# | ShiftLeft2 | Wait #FrameInterval# | ShiftLeft3 | Wait #FrameInterval# | BlankSlot | Wait #FrameInterval# | ResetVertical | Launch
+ActionList3=ResetVertical | Wait #FrameInterval# | ShiftRight1 | Wait #FrameInterval# | ShiftRight2 | Wait #FrameInterval# | ShiftRight3 | Wait #FrameInterval# | BlankSlot | Wait #FrameInterval# | ResetVertical | Launch
+ActionList4=ResetHorizontal | Wait #FrameInterval# | Shrink1Horizontal | Wait #FrameInterval# | Shrink2Horizontal | Wait #FrameInterval# | Shrink3Horizontal | Wait #FrameInterval# | BlankSlot | Wait #FrameInterval# | ResetHorizontal | Launch
+ActionList5=ResetHorizontal | Wait #FrameInterval# | ShiftUp1 | Wait #FrameInterval# | ShiftUp2 | Wait #FrameInterval# | ShiftUp3 | Wait #FrameInterval# | BlankSlot | Wait #FrameInterval# | ResetHorizontal | Launch
+ActionList6=ResetHorizontal | Wait #FrameInterval# | ShiftDown1 | Wait #FrameInterval# | ShiftDown2 | Wait #FrameInterval# | ShiftDown3 | Wait #FrameInterval# | BlankSlot | Wait #FrameInterval# | ResetHorizontal | Launch
+Launch=[!CommandMeasure LauhdutinScript "Launch('#SlotToAnimate#')"]
+BlankSlot=[!SetVariable "SlotImage#SlotToAnimate#" ""][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]""")
+
+        f.write("""
+ResetVertical=[!SetOption "SlotBanner#SlotToAnimate#" "X" "0"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "((#SlotToAnimate# - 1) * %s)"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ResetHorizontal=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "0"][!SetOption "SlotBanner#SlotToAnimate#" "X" "((#SlotToAnimate# - 1) * %s)"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]""" %
+    (
+        SLOT_HEIGHT, SLOT_WIDTH, SLOT_HEIGHT,
+        SLOT_WIDTH, SLOT_WIDTH, SLOT_HEIGHT
+    )
+)
+
+        ShrinkLevel1Width = SLOT_WIDTH / 1.8
+        ShrinkLevel2Width = SLOT_WIDTH / 4
+        ShrinkLevel3Width = SLOT_WIDTH / 20
+        ShrinkLevel1Height = SLOT_HEIGHT / 1.8
+        ShrinkLevel2Height = SLOT_HEIGHT / 4
+        ShrinkLevel3Height = SLOT_HEIGHT / 20
+        f.write("""
+Shrink1Vertical=[!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "(((#SlotToAnimate# - 1) * %s) + %s)"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shrink2Vertical=[!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "(((#SlotToAnimate# - 1) * %s) + %s)"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shrink3Vertical=[!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "(((#SlotToAnimate# - 1) * %s) + %s)"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]""" %
+    (
+        ((SLOT_WIDTH - ShrinkLevel1Width) / 2), SLOT_HEIGHT, ((SLOT_HEIGHT - ShrinkLevel1Height) / 2), (ShrinkLevel1Width), (ShrinkLevel1Height),
+        ((SLOT_WIDTH - ShrinkLevel2Width) / 2), SLOT_HEIGHT, ((SLOT_HEIGHT - ShrinkLevel2Height) / 2), (ShrinkLevel2Width), (ShrinkLevel2Height),
+        ((SLOT_WIDTH - ShrinkLevel3Width) / 2), SLOT_HEIGHT, ((SLOT_HEIGHT - ShrinkLevel3Height) / 2), (ShrinkLevel3Width), (ShrinkLevel3Height)
+    )
+)
+
+        f.write("""
+Shrink1Horizontal=[!SetOption "SlotBanner#SlotToAnimate#" "X" "(((#SlotToAnimate# - 1) * %s) + %s)"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shrink2Horizontal=[!SetOption "SlotBanner#SlotToAnimate#" "X" "(((#SlotToAnimate# - 1) * %s) + %s)"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shrink3Horizontal=[!SetOption "SlotBanner#SlotToAnimate#" "X" "(((#SlotToAnimate# - 1) * %s) + %s)"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]""" %
+    (
+        SLOT_WIDTH, ((SLOT_WIDTH - ShrinkLevel1Width) / 2), ((SLOT_HEIGHT - ShrinkLevel1Height) / 2), (ShrinkLevel1Width), (ShrinkLevel1Height),
+        SLOT_WIDTH, ((SLOT_WIDTH - ShrinkLevel2Width) / 2), ((SLOT_HEIGHT - ShrinkLevel2Height) / 2), (ShrinkLevel2Width), (ShrinkLevel2Height),
+        SLOT_WIDTH, ((SLOT_WIDTH - ShrinkLevel3Width) / 2), ((SLOT_HEIGHT - ShrinkLevel3Height) / 2), (ShrinkLevel3Width), (ShrinkLevel3Height)
+    )
+)
+
+        ShiftLevel1Vertical = (SLOT_WIDTH / 20.0)
+        ShiftLevel2Vertical = (SLOT_WIDTH / 4.0)
+        ShiftLevel3Vertical = (SLOT_WIDTH / 1.8)
+        ShiftLevel1Horizontal = (SLOT_HEIGHT / 20.0)
+        ShiftLevel2Horizontal = (SLOT_HEIGHT / 4.0)
+        ShiftLevel3Horizontal = (SLOT_HEIGHT / 1.8)
+        f.write("""
+ShiftLeft1=[!SetOption "SlotBanner#SlotToAnimate#" "X" "(0 - %s)"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftLeft2=[!SetOption "SlotBanner#SlotToAnimate#" "X" "(0 - %s)"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftLeft3=[!SetOption "SlotBanner#SlotToAnimate#" "X" "(0 - %s)"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftRight1=[!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftRight2=[!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftRight3=[!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftUp1=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "(0 - %s)"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftUp2=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "(0 - %s)"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftUp3=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "(0 - %s)"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftDown1=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftDown2=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ShiftDown3=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+DynamicVariables=1
+""" %
+    (
+        ShiftLevel1Vertical,
+        ShiftLevel2Vertical,
+        ShiftLevel3Vertical,
+        ShiftLevel1Vertical,
+        ShiftLevel2Vertical,
+        ShiftLevel3Vertical,
+        ShiftLevel1Horizontal,
+        ShiftLevel2Horizontal,
+        ShiftLevel3Horizontal,
+        ShiftLevel1Horizontal,
+        ShiftLevel2Horizontal,
+        ShiftLevel3Horizontal
+    )
+)
+
+        f.write("""
+[HoverOnAnimation]
+Measure=Plugin
+Plugin=ActionTimer
+DynamicVariables=1
+ActionList1=ZoomIn1Vertical | Wait #FrameInterval# | ZoomIn2Vertical | Wait #FrameInterval# | ZoomIn3Vertical
+ActionList2=ZoomIn1Horizontal | Wait #FrameInterval# | ZoomIn2Horizontal | Wait #FrameInterval# | ZoomIn3Horizontal
+ActionList3=Jiggle1 | Wait #FrameInterval# | Jiggle2 | Wait #FrameInterval# | Jiggle3 | Wait #FrameInterval# | Jiggle2
+ActionList4=Shake1 | Wait #FrameInterval# | Shake2 | Wait #FrameInterval# | Shake3 | Wait #FrameInterval# | Shake2""")
+
+        f.write("""
+ZoomIn1Vertical=[!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ZoomIn2Vertical=[!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ZoomIn3Vertical=[!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "X" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]""" %
+    (
+        (SLOT_WIDTH / 100 * 105), (0 - ((SLOT_WIDTH / 100 * 105) - SLOT_WIDTH) / 2),
+        (SLOT_WIDTH / 100 * 110), (0 - ((SLOT_WIDTH / 100 * 110) - SLOT_WIDTH) / 2),
+        (SLOT_WIDTH / 100 * 115), (0 - ((SLOT_WIDTH / 100 * 115) - SLOT_WIDTH) / 2)
+    )
+)
+
+        f.write("""
+ZoomIn1Horizontal=[!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ZoomIn2Horizontal=[!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ZoomIn3Horizontal=[!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Jiggle1=[!SetOption "SlotBanner#SlotToAnimate#" "ImageRotate" "2"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Jiggle2=[!SetOption "SlotBanner#SlotToAnimate#" "ImageRotate" "0"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Jiggle3=[!SetOption "SlotBanner#SlotToAnimate#" "ImageRotate" "-2"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]""" %
+    (
+        (SLOT_HEIGHT / 100 * 105), (0 - ((SLOT_HEIGHT / 100 * 105) - SLOT_HEIGHT) / 2),
+        (SLOT_HEIGHT / 100 * 110), (0 - ((SLOT_HEIGHT / 100 * 110) - SLOT_HEIGHT) / 2),
+        (SLOT_HEIGHT / 100 * 115), (0 - ((SLOT_HEIGHT / 100 * 115) - SLOT_HEIGHT) / 2)
+    )
+)
+
+        if ORIENTATION == "vertical":
+            f.write("""
+Shake1=[!SetOption "SlotBanner#SlotToAnimate#" "X" "-5"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shake2=[!SetOption "SlotBanner#SlotToAnimate#" "X" "0"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shake3=[!SetOption "SlotBanner#SlotToAnimate#" "X" "5"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+""")
+        else:
+            f.write("""
+Shake1=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "-5"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shake2=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "0"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+Shake3=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "5"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+""")
+
+        f.write("""
+[HoverOffAnimation]
+Measure=Plugin
+Plugin=ActionTimer
+ActionList1=ResetVertical
+ActionList2=ResetHorizontal
+ResetVertical=[!SetOption "SlotBanner#SlotToAnimate#" "X" "0"][!SetOption "SlotBanner#SlotToAnimate#" "Y" "((#SlotToAnimate# - 1) * %s)"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+ResetHorizontal=[!SetOption "SlotBanner#SlotToAnimate#" "Y" "0"][!SetOption "SlotBanner#SlotToAnimate#" "X" "((#SlotToAnimate# - 1) * %s)"][!SetOption "SlotBanner#SlotToAnimate#" "W" "%s"][!SetOption "SlotBanner#SlotToAnimate#" "H" "%s"][!UpdateMeter "SlotBanner#SlotToAnimate#"][!Redraw]
+DynamicVariables=1
+""" % (
+        SLOT_HEIGHT, SLOT_WIDTH, SLOT_HEIGHT,
+        SLOT_WIDTH, SLOT_WIDTH, SLOT_HEIGHT
+    )
+)
 
         # Slot background
         f.write("""
@@ -50,125 +212,206 @@ Meter=Image
 X=0
 Y=0
 SolidColor=#SlotBackgroundColor#""")
-        if settings.get("orientation", "vertical") == "vertical":
+        if ORIENTATION == "vertical":
             f.write("""
-W=#SlotWidth#
-H=(#SlotCount#*#SlotHeight#)
-""")
+W=%s
+H=%s
+""" % (
+        SLOT_WIDTH,
+        SLOT_COUNT * SLOT_HEIGHT
+    )
+)
         else:
             f.write("""
-W=(#SlotCount#*#SlotWidth#)
-H=#SlotHeight#
-""")
+W=%s
+H=%s
+""" % (
+        SLOT_COUNT * SLOT_WIDTH,
+        SLOT_HEIGHT
+    )
+)
 
         # Slots
         i = 1
-        while i <= int(settings.get("slot_count", 6)):
+        while i <= SLOT_COUNT:
             # Text for game title
             f.write("""
 [SlotText%s]
 Meter=String""" % i)
 
-            if settings.get("orientation", "vertical") == "vertical":
+            if ORIENTATION == "vertical":
                 f.write("""
-X=(#SlotWidth#/2)
-Y=(%s*#SlotHeight#+#SlotHeight#/2)""" % (i - 1))
+X=%s
+Y=%s""" % (
+        SLOT_WIDTH / 2,
+        (i - 1) * SLOT_HEIGHT + SLOT_HEIGHT / 2
+    )
+)
             else:
                 f.write("""
-X=(%s*#SlotWidth#+#SlotWidth#/2)
-Y=(#SlotHeight#/2)""" % (i - 1))
+X=%s
+Y=%s""" % (
+        (i - 1) * SLOT_WIDTH + SLOT_WIDTH / 2,
+        SLOT_HEIGHT / 2
+    )
+)
 
             f.write("""
-W=#SlotWidth#
-H=#SlotHeight#
+W=%s
+H=%s
 Text=#SlotName%s#
 FontFace=Arial
-FontSize=(#SlotWidth#/15)
-FontColor=#SlotTextColor#
+FontSize=%s
+FontColor=%s
 StringAlign=CenterCenter
 StringEffect=Shadow
 ClipString=1
 AntiAlias=1
 DynamicVariables=1
 Group=Slots
-""" % i)
+""" % (
+        SLOT_WIDTH,
+        SLOT_HEIGHT,
+        i,
+        SLOT_WIDTH / 15,
+        settings.get("slot_text_color", "255,255,255,255")
+    )
+)
 
             # Game banner
             f.write("""
 [SlotBanner%s]
 Meter=Image
 ImageName=#SlotImage%s#""" % (i, i))
-            if settings.get("orientation", "vertical") == "vertical":
+            if ORIENTATION == "vertical":
                 f.write("""
 X=0
-Y=(%s*#SlotHeight#)""" % (i - 1))
+Y=%s""" % (
+        (i - 1) * SLOT_HEIGHT
+    )
+)
             else:
                 f.write("""
-X=(%s*#SlotWidth#)
-Y=0""" % (i - 1))
+X=%s
+Y=0""" % (
+        (i - 1) * SLOT_WIDTH
+    )
+)
 
             f.write("""
-W=#SlotWidth#
-H=#SlotHeight#
+W=%s
+H=%s
 SolidColor=0,0,0,1
 PreserveAspectRatio=2
 DynamicVariables=1
 MouseOverAction=[!CommandMeasure LauhdutinScript "Highlight('%s')"]
 MouseLeaveAction=[!CommandMeasure LauhdutinScript "Unhighlight('%s')"]
-LeftMouseUpAction=[!CommandMeasure LauhdutinScript "Launch('%s')"]
+""" % (
+        SLOT_WIDTH,
+        SLOT_HEIGHT,
+        i,
+        i
+    )
+)
+
+            if settings.get("click_animation", 1) > 0:
+                f.write("""LeftMouseUpAction=[!SetVariable "SlotToAnimate" "%s"][!UpdateMeasure "ClickAnimation"][!CommandMeasure "ClickAnimation" "Execute %s"]
 Group=Slots
-""" % (i, i, i))
+""" % (i, settings.get("click_animation", 1)))
+            else:
+                f.write("""LeftMouseUpAction=[!CommandMeasure LauhdutinScript "Launch('%s')"]
+Group=Slots
+""" % i)
 
             # Game highlight
             f.write("""
 [SlotHighlightBackground%s]
-Meter=Image
-X=0r
-Y=0r
-W=#SlotWidth#
-H=#SlotHeight#
+Meter=Image""" % i)
+            if ORIENTATION == "vertical":
+                f.write("""
+X=0
+Y=%s""" % (
+        (i - 1) * SLOT_HEIGHT
+    )
+)
+            else:
+                f.write("""
+X=%s
+Y=0""" % (
+        (i - 1) * SLOT_WIDTH
+    )
+)
+
+            f.write("""
+W=%s
+H=%s
 SolidColor=0,0,0,160
 PreserveAspectRatio=2
 DynamicVariables=1
 Group=SlotHighlight%s
+""" % (
+        SLOT_WIDTH,
+        SLOT_HEIGHT,
+        i
+    )
+)
 
+            f.write("""
 [SlotHighlight%s]
 Meter=Image
 ImageName=
 X=0r
 Y=0r
-W=#SlotWidth#
-H=#SlotHeight#
+W=%s
+H=%s
 SolidColor=0,0,0,1
 PreserveAspectRatio=2
 DynamicVariables=1
 Group=SlotHighlight%s
+""" % (
+        i,
+        SLOT_WIDTH,
+        SLOT_HEIGHT,
+        i
+    )
+)
 
+            f.write("""
 [SlotHighlightText%s]
 Meter=String
-X=(#SlotWidth# / 2)r
-Y=(#SlotHeight# / 2)r
-W=#SlotWidth#
-H=#SlotHeight#
+X=%sr
+Y=%sr
+W=%s
+H=%s
 Text=#SlotHighlightMessage%s#
 FontFace=Arial
-FontSize=(#SlotWidth#/25)
-FontColor=#SlotTextColor#
+FontSize=%s
+FontColor=%s
 StringAlign=CenterCenter
 StringEffect=Shadow
 ClipString=1
 AntiAlias=1
 DynamicVariables=1
 Group=SlotHighlight%s
-""" % (i, i, i, i, i, i, i))
-
+""" % (
+        i,
+        SLOT_WIDTH / 2,
+        SLOT_HEIGHT / 2,
+        SLOT_WIDTH,
+        SLOT_HEIGHT,
+        i,
+        SLOT_WIDTH / 25,
+        settings.get("slot_text_color", "255,255,255,255"),
+        i
+    )
+)
             i += 1
 
     if CurrentFile != "Main.ini":
         subprocess.call(
-            [RainmeterPath, "!ActivateConfig", Config, "Main.ini"], shell=True)
+            [RainmeterPath, "!ActivateConfig", Config, "Main.ini"], shell=False)
     else:
-        subprocess.call([RainmeterPath, "!Refresh", Config], shell=True)
+        subprocess.call([RainmeterPath, "!Refresh", Config], shell=False)
 except:
     import traceback
     traceback.print_exc()
--- Readme.md
+++ Readme.md
@@ -1,8 +1,10 @@
 Lauhdutin
 ==
-A Rainmeter skin for launching games. Supports Steam, GOG Galaxy, Battle.net, and regular Windows shortcuts.
+A Rainmeter skin for aggregating games from different platforms and then launching them. Supports Steam, GOG Galaxy, Battle.net, and regular Windows shortcuts. Games are presented as a scrollable list that can be filtered and sorted in multiple ways. There are a variety of setting that allow you to customize the appearance of the skin (e.g. orientation, number of slots, dimensions of a slot, animations).
 
-![ex](demo.gif)
+![ex](Docs/demo.gif)
+
+[Higher resolution version of the animated GIF](Docs/Demo-original.gif)
 
 # Contents
  - [Requirements](#requirements)
@@ -11,6 +13,9 @@ A Rainmeter skin for launching games. Supports Steam, GOG Galaxy, Battle.net, an
  - [Supported platforms](#supported-platforms)
  - [Filtering](#filtering)
  - [Bangs](#bangs)
+ - [Animations](#animations)
+ - [Reporting issues](#reporting-issues)
+ - [Contributing](#contributing)
  - [Changelog](#changelog)
  - [License](#license)
 
@@ -53,9 +58,9 @@ or
 - Load **Settings.ini** in Rainmeter, click **Save**, click **Exit**, right-click on the skin, go to **Custom skin actions**, and click on **Rebuild**.
 
 # Supported platforms
-Lauhdutin currently supports Steam, GOG Galaxy, and Battle.net.
 
-Steam support includes:
+## Steam
+Support includes:
 - Acquire a list of installed games and games that are not currently installed, but for which a license has been purchased.
 - Acquire a list of games that have been added to Steam as a 'non-Steam game'.
 - Launch the games that were found by the features described above.
@@ -63,12 +68,14 @@ Steam support includes:
 - Automatically download banners for Steam games that were found.
 - Integrate the total amount of hours played that is tracked by Steam into Lauhdutin's corresponding system.
 
-GOG Galaxy support includes:
+## GOG Galaxy
+Support includes:
 - Acquire a list of games installed via GOG Galaxy.
 - Launch games that were found.
 - Automatically download banners for games that were found.
 
-Battle.net support includes:
+## Battle.net
+Support includes:
 - Acquire a list of games installed via Battle.net.
 - Launch games that were found.
 - Automatically download banners for games that were found.
@@ -77,7 +84,7 @@ Battle.net support does not include support for classic games (e.g. Diablo II, W
 - The name of the game's folder.
 - The path to the executable relative to the root of the game folder (e.g. `D:\Games\Some game\bin\game.exe` would become `\bin\game.exe`)
 
-Even better support for the platforms listed above may be implemented in the future, when possible.
+## Other platforms
 
 Additional platforms may receive similar support in the future, if possible. In the mean time it is possible to add games, which were not installed via the supported platforms described above, by placing a shortcut in `\Rainmeter\Skins\Lauhdutin\@Resources\Shortcuts` (banners can be placed in `\Rainmeter\Skins\Lauhdutin\@Resources\Banners\Shortcuts` with the same name as the shortcut).
 
@@ -116,7 +123,74 @@ Currently supported events that can be used to trigger the execution of bangs:
 
 The stopping bang can also be executed manually via the context menu, if the skin fails to automatically execute it when a game stops running.
 
+# Highlighting
+
+If highlighting is enabled, then additional contextual information can be shown when the mouse cursor is hovered over a slot ([animated example](Docs/Highlighting.gif)). There are some settings for toggling certain pieces of information (e.g. platform, hours played).
+
+# Animations
+
+## Clicking
+One of these animations can be played when a slot is left-clicked ([animated example](Docs/ClickAnimations.gif)):
+
+- Shift left
+- Shift up
+- Shift right
+- Shift down
+- Shrink
+
+Click animations can be disabled completely.
+
+## Hovering
+One of these animations can be played when the mouse cursor hovers over a slot ([animated example](Docs/HoverAnimations.gif)): 
+
+- Zoom in
+- Jiggle
+- Shake
+
+Hover animations can be disabled completely.
+
+Note that some animations may not work properly if the slot's aspect ratio differs significantly from the banner's aspect ratio. This can be an issue e.g. with the `Zoom in` hover animation when the skin is in horizontal mode.
+
+# Reporting issues
+If you encounter an issue while trying to use Lauhdutin, then please read through the readme in case there is an explanation on how to deal with the issue.
+
+If the issue persists, then check through the repository's [Issues](https://github.com/Kapiainen/Lauhdutin/issues) section for open or closed issues that might be relevant and post there (or reference that issue when contacting outside of GitHub).
+
+If there is no previously submitted issue that matches your issue, then submit an issue report based on this [template](https://github.com/Kapiainen/Lauhdutin/blob/master/.github/ISSUE_TEMPLATE.md) (check the raw version for comments with more detailed steps).
+
+# Contributing
+
+Fork [this](https://github.com/Kapiainen/Lauhdutin) repository, make your changes, and submit a pull request with a summary of the changes you've made.
+
+Try to include tests and mock data for those tests. These tests should preferrably be integrated into the build system that is used to generate releases.
+
+Try to keep the number of dependencies, which cannot be included in the skin or are not a part of a default Windows installation, to a minimum.
+
+## Graphical user interface changes
+Try to keep draw calls to a minimum by, for example:
+- Not overriding the skin-wide `DefaultUpdateDivider=-1` option.
+- Executing the `!Redraw` bang only when necessary (e.g. update all meter options prior to a draw call instead of setting a few options, drawing, and then setting the rest of the options).
+
+## Adding support for a platform
+There are a few rules that **must** be followed when adding support for additional platforms:
+- Sensitive account data must never be transmitted from the user's system. Preferably the only transmitted data should be relevant to downloading e.g. banners.
+- All data that is retrieved and/or utilized by the skin must have been intentionally made publicly available by the platform's developers and/or stored locally in an unencrypted state.
+- Local data should be preferred over data that needs to be acquired over the internet.
+- Minimize internet usage by e.g. downloading a banner only if a local copy does not already exist.
+
+Any deviations from the rules regarding adding platform support will most likely result in a rejected pull request.
+
 # Changelog
+
+**Version 2.5.0 - 2017/03/06:**
+- Added optional animations when clicking on a slot.
+- Added optional animations when hovering the mouse over a slot.
+- Added support for retaining information about games, which were previously detected and then uninstalled, for future use.
+- Added button separators to the toolbar.
+- Added 'R' next to the sorting icon when the sorting order is reversed.
+- Updated toolbar icons.
+- Updated settings skin.
+
 **Version 2.4.0 - 2017/03/01:**
 - Updated sorting of most recently played games to sort them alphabetically when timestamps are equal.
 - Added sorting by total hours played.
