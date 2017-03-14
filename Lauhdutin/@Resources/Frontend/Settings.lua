c46581e2c999157ea94dbfec069f14ed88762228
BanCrash <BanCrash@users.noreply.github.com>
2017-03-14 21:30:19 +0100

    Update Settings.lua

--- Lauhdutin/@Resources/Frontend/Settings.lua
+++ Lauhdutin/@Resources/Frontend/Settings.lua
@@ -130,7 +130,7 @@ function Exit()
 	if REBUILD_SWITCH then
 		SKIN:Bang('["#Python#" "#@#Frontend\\BuildSkin.py" "#PROGRAMPATH#;" "#@#;" "#CURRENTCONFIG#;" "#CURRENTFILE#;"]')
 	else
-		SKIN:Bang('[!ActivateConfig #CURRENTCONFIG# "Main.ini"]')
+		SKIN:Bang('[!ActivateConfig "#CURRENTCONFIG#" "Main.ini"]')
 	end	
 end
 
