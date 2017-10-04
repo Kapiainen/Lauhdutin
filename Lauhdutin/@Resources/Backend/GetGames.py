e7b26b2cfd3ce8942a779d31865d2f5c2457c9d9
BanCrash <BanCrash@users.noreply.github.com>
2017-10-04 12:37:35 +0200

    Rearranged code

--- Lauhdutin/@Resources/Backend/GetGames.py
+++ Lauhdutin/@Resources/Backend/GetGames.py
@@ -149,13 +149,12 @@ try:
             i = 1
             backups = 5
             while i <= backups:
+                if i < 10:
+                    all_games_old = read_json(os.path.join(ResourcePath, "games_daily_backup_0%s.json") % (i)) 
+                else:
+                    all_games_old = read_json(os.path.join(ResourcePath, "games_daily_backup_%s.json") % (i))
                 if all_games_old:
                     break
-                else:
-                    if i < 10:
-                        all_games_old = read_json(os.path.join(ResourcePath, "games_daily_backup_0%s.json") % (i)) 
-                    else:
-                        all_games_old = read_json(os.path.join(ResourcePath, "games_daily_backup_%s.json") % (i))
                 i += 1
         if all_games_old:
             for game_new in all_games:
