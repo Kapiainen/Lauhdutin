6faa4408269fd0a8bc143f2d804c4d58aa8e0986
Kapiainen <Kapiainen@users.noreply.github.com>
2018-05-22 12:31:01 +0300

    Updated Library class
    - Games are now saved along with the modification time in addition to
    the file format version.
    - Games are only detected if the previous modification date differs from
    the current date.

--- dist/@Resources/shared/library.lua
+++ dist/@Resources/shared/library.lua
@@ -70,6 +70,9 @@ local Library
 do
   local _class_0
   local _base_0 = {
+    getDetectGames = function(self)
+      return self.detectGames
+    end,
     createBackup = function(self, path)
       local games = io.readJSON(path)
       local date = os.date('*t')
@@ -148,7 +151,8 @@ do
       end
       return io.writeJSON(self.path, {
         version = self.version,
-        games = games
+        games = games,
+        modified = os.date('*t')
       })
     end,
     migrate = function(self, games, version)
@@ -597,14 +601,41 @@ do
       assert(type(regularMode) == 'boolean', 'shared.library.Library')
       self.version = 1
       self.path = 'games.json'
-      if regularMode then
-        self.numBackups = settings:getNumberOfBackups()
-        self.backupFilePattern = 'games_backup_%d.json'
-        self.games = { }
-        self.oldGames = self:load()
-        self.currentGameID = 1
+      local games
+      if io.fileExists(self.path) then
+        games = io.readJSON(self.path)
       else
-        local games = io.readJSON(self.path)
+        games = { }
+      end
+      self.currentGameID = 1
+      self.numBackups = settings:getNumberOfBackups()
+      self.backupFilePattern = 'games_backup_%d.json'
+      self.filterStack = { }
+      self.processedGames = nil
+      self.gamesSortedByGameID = nil
+      self.detectGames = false
+      if regularMode then
+        local date = os.date('*t')
+        local modified = games.modified or { }
+        if modified.year == date.year and modified.month == date.month and modified.day == date.day then
+          do
+            local _accum_0 = { }
+            local _len_0 = 1
+            local _list_0 = games.games
+            for _index_0 = 1, #_list_0 do
+              local args = _list_0[_index_0]
+              _accum_0[_len_0] = Game(args)
+              _len_0 = _len_0 + 1
+            end
+            self.games = _accum_0
+          end
+          self.oldGames = { }
+        else
+          self.games = { }
+          self.oldGames = self:load()
+          self.detectGames = true
+        end
+      else
         do
           local _accum_0 = { }
           local _len_0 = 1
@@ -618,9 +649,6 @@ do
         end
         self.oldGames = { }
       end
-      self.filterStack = { }
-      self.processedGames = nil
-      self.gamesSortedByGameID = nil
     end,
     __base = _base_0,
     __name = "Library"
